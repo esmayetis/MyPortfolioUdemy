854cb1fc88a79628421b8e1963f56f97ea054605
Esma  Yetiş <100996071+esmayetis@users.noreply.github.com>
2024-07-22 00:18:01 +0300

    ikinci update

--- MyPortfolioUdemy/DAL/Entities/Portfolio.cs
+++ MyPortfolioUdemy/DAL/Entities/Portfolio.cs
@@ -2,7 +2,7 @@
 {
 	public class Portfolio
 	{
-        public int PortfoliId { get; set; }
+        public int PortfolioId { get; set; }
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string ImageUrl { get; set; }
