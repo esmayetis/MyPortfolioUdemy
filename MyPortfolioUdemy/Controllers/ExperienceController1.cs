c45395293ea7b18c8a28c06b4fb2ad4fc182f074
Esma  Yetiş <100996071+esmayetis@users.noreply.github.com>
2024-07-29 00:26:07 +0300

    dördüncü update

---
+++ MyPortfolioUdemy/Controllers/ExperienceController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class ExperienceController1 : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();  
+        public IActionResult ExperienceList()
+        {
+            var values= context.Experiences.ToList();
+            return View(values);
+        }
+    }
+}
